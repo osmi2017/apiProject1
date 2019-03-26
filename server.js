aaffa081f7e1f4776f625c0d0a363fa7e4d9c50e
Glitch (dandy-lyre) <none>
2019-03-26 03:20:27 +0000

    🚅🍕 Checkpoint ./server.js:3394133/1942

--- server.js
+++ server.js
@@ -1,21 +1,64 @@
-// server.js
-// where your node app starts
+'use strict';
 
-// init project
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
-var apps = module.exports = express();
-app.user(bodyParser.json());
+
 app.use(cors());
 
+app.use('/public', express.static(process.cwd() + '/public'));
 
+app.route('/')
+  .get(function (req, res) {
+    res.sendFile(process.cwd() + '/views/index.html');
+  });
+    
+app.route('/api/timestamp/:date?')
+  .get(function (req, res){
+    
+    var date = null;
+    // parse the date string
+    if (req.params.date !== undefined) {
 
-// listen for requests :)
-app.listen(3000, function(){
-  console.log('working');
+      // check if it is a unix timestamp...
+      var unixTimestamp = parseInt(req.params.date*1);
+      if (isNaN(unixTimestamp)) {
+        
+        // it's not a unix timestamp string
+        date = new Date(req.params.date);
+      } else {
+        
+        // it is a timestamp
+        date = new Date(unixTimestamp);
+      }
+      
+    } else {
+      
+      // the date string parameter is empty. 
+      // create a new date based on current time 
+      date = new Date(Date.now());
+    }
+    
+    // Initialize the response object, if Date is invalid
+    // this one will be returned
+
+    var response = date == "Invalid Date" ? 
+      { error: "Invalid Date" } :
+      { "unix": date.getTime(),
+        "utc": date.toUTCString()
+      };
+    
+    res.json(response);
+  });
+    
+// 404 Not Found Middleware
+app.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
 });
